758193c1536c1703d0d1d87dd8dc3fff8fd27062
JHH <gusgh9098@naver.com>
2024-03-06 17:17:06 +0900

    4강

---
+++ src/components/B.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Message = ({ message }) => {
+    return <p>{ message }</p>
+}
+
+const ListItem = ({ post }) => {
+    return(
+        <li key={post.id}>
+            <p>{post.title}</p>
+        </li>
+    )
+}
+
+const List = ({ posts }) =>{
+    return(
+        <ul>
+            {posts.map(post => (
+                <ListItem key={post.id} post={post} />
+            ))}
+        </ul>
+    )
+}
+
+const B = ({ message, posts}) => {
+  return (
+    <div>
+        <h1>B Components</h1>
+        <Message message = {message} />
+        <List posts ={posts} />
+    </div>
+  )
+}
+
+export default B
